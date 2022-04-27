2604ebcd400d71e046c5b9b2044cc0b8f035f2be
Caleb Eggensperger <calebegg@google.com>
2022-04-27 10:38:49 -0400

    Remove unused code

--- src/editor/proofbar.ts
+++ src/editor/proofbar.ts
@@ -18,7 +18,12 @@
 import { syntaxTree } from "@codemirror/language";
 import { EditorView, gutter, GutterMarker } from "@codemirror/view";
 import { Acl2Response, evaluate } from "../acl2_driver";
-import { StateField, StateEffect, RangeSet } from "@codemirror/state";
+import {
+  StateField,
+  StateEffect,
+  RangeSet,
+  EditorState,
+} from "@codemirror/state";
 
 class FormMarker extends GutterMarker {
   constructor(
@@ -42,34 +47,16 @@ class FormMarker extends GutterMarker {
 }
 
 export function proofBar(onOutput: (response: Acl2Response) => void) {
-  return gutter({
-    class: "proof-bar",
-    //   lineMarker(view, line) {
-    //     let node = syntaxTree(view.state).resolve(line.from, 1);
-    //     let prevNode = node;
-    //     if (node.type.name === "Document") return null;
-    //     let depth = 0;
-    //     const lineStart = getLineNo(view, line.from);
-    //     do {
-    //       const content = view.state.sliceDoc(node.from, node.to);
-    //       if (node.name === "bracket" && content === "(") {
-    //         depth++;
-    //       } else if (node.name === "bracket" && content === ")") {
-    //         depth--;
-    //       }
-    //       prevNode = node;
-    //       node = node.nextSibling!;
-    //     } while (depth > 0 && node);
-    //     let height = view.viewportLineBlocks.length - lineStart;
-    //     if (node) height = getLineNo(view, node.from) - lineStart;
-    //     return new FormMarker(
-    //       onOutput,
-    //       height,
-    //       view.state.sliceDoc(0, prevNode.to),
-    //     );
-    //   },
-    initialSpacer: () => new FormMarker(onOutput, 0, ""),
-  });
+  return [
+    EditorState.changeFilter.of(() => {
+      // TODO(calebegg): Mark parts of the document readonly
+      return [0, 0];
+    }),
+    gutter({
+      class: "proof-bar",
+      initialSpacer: () => new FormMarker(onOutput, 0, ""),
+    }),
+  ];
 }
 
 const proofBarState = StateField.define<RangeSet<GutterMarker>>({
@@ -81,29 +68,7 @@ const proofBarState = StateField.define<RangeSet<GutterMarker>>({
     console.log(transaction);
     for (let e of transaction.effects) {
       console.log(e);
-      // if (e.is(proofBarEffect)) {
-      //   if (e.value.on)
-      //     set = set.update({ add: [breakpointMarker.range(e.value.pos)] });
-      //   else
-      //     set = set.update({ filter: (from: number) => from != e.value.pos });
-      // }
     }
     return set;
   },
 });
-
-function toggleBreakpoint(view: EditorView, pos: number) {
-  let breakpoints = view.state.field(proofBarState);
-  let hasBreakpoint = false;
-  breakpoints.between(pos, pos, () => {
-    hasBreakpoint = true;
-  });
-  view.dispatch({
-    // effects: proofBarEffect.of({ pos, on: !hasBreakpoint }),
-  });
-}
-
-function getLineNo(view: EditorView, pos: number) {
-  const lineBlock = view.lineBlockAt(pos);
-  return lineBlock.top / lineBlock.height;
-}
